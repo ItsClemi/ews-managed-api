76e145d9ec52635dceaafa0c144824b1bfa087c2
Clemens Susenbeth <clemens@susenbeth.com>
2025-05-13 00:55:26 +0200

    Added strong name signing to both libraries

--- src/Exchange.WebServices.NETCore/Properties/AssemblyInfoOpenSource.cs
+++ src/Exchange.WebServices.NETCore/Properties/AssemblyInfoOpenSource.cs
@@ -26,7 +26,10 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("Exchange.WebServices.NETCore.Autodiscover")]
+[assembly:
+    InternalsVisibleTo(
+        "Exchange.WebServices.NETCore.Autodiscover, PublicKey=00240000048000009400000006020000002400005253413100040000010001007911ed4d7b321d6404bd527954c32c659e4b88fe418e002751d0ed08750359e07fdf99e0a7d1e13ca131799378f0e5a849fabc95c735f7067a63cf1a4b48603ebef93221948f8d61771d4af2391f29fcafabd6576988da6b5ba2fcb822dbb7a0a0cda6f59d150d655bfd61d53348264916d4c6998f3cff1902a726fd08be19db"
+    )]
 
 
 // Assembly information
